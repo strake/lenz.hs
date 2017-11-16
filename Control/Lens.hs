6a1e60a5764ea1a90c61e320bb74be8c206c11e3
M Farkas-Dyck <strake888@gmail.com>
2017-11-15 17:55:38 -0800

    iso

--- Control/Lens.hs
+++ Control/Lens.hs
@@ -1,6 +1,6 @@
 module Control.Lens (Lens, Refractor, Traversal,
                      lens, iso,
-                     get, set, modify,
+                     get, set, modify, mapping,
                      fstL, sndL, swapL, unitL, bitL) where
 
 import Prelude hiding (id)
@@ -12,6 +12,7 @@ import Control.Category.Unicode
 import Data.Bits (Bits (..))
 import Data.Bool (bool)
 import Data.Functor.Identity
+import Data.Profunctor
 import Data.Tuple (swap)
 
 type Refractor c d α β a b = ∀ p f . (d f, c p) ⇒ p a (f b) → p α (f β)
@@ -20,11 +21,13 @@ type Lens α β a b = Refractor ((~) (→)) Functor α β a b
 
 type Traversal α β a b = Refractor ((~) (→)) Applicative α β a b
 
+type Iso α β a b = Refractor Profunctor Functor α β a b
+
 lens :: (α → a) → (b → α → β) → Lens α β a b
 lens get set ret = liftA2 fmap (flip set) (ret ∘ get)
 
-iso :: (α → a) → (b → β) → Lens α β a b
-iso f g = (fmap g ∘) ∘ (∘ f)
+iso :: (α → a) → (b → β) → Iso α β a b
+iso f g = dimap f (fmap g)
 
 get :: ((a → Const a b) → α → Const a β) → α → a
 get l = getConst ∘ l Const
@@ -35,13 +38,25 @@ set l = modify l ∘ pure
 modify :: ((a → Identity b) → α → Identity β) → (a → b) → α → β
 modify l f = runIdentity ∘ l (Identity ∘ f)
 
+mapping :: (Functor f, Functor g) => AnIso α β a b -> Iso (f α) (g β) (f a) (g b)
+mapping = (`withIso` \ f g -> iso (fmap f) (fmap g))
+
+withIso :: AnIso α β a b -> ((α -> a) -> (b -> β) -> c) -> c
+withIso x = case x (Xchg id Identity) of Xchg φ φ' -> \ f -> f φ (runIdentity ∘ φ')
+
+type AnIso α β a b = Xchg a b a (Identity b) -> Xchg a b α (Identity β)
+
+data Xchg a b α β = Xchg (α -> a) (b -> β) deriving (Functor)
+
+instance Profunctor (Xchg a b) where dimap f g (Xchg φ φ') = Xchg (φ ∘ f) (g ∘ φ')
+
 fstL :: Lens (a, c) (b, c) a b
 fstL = swapL ∘ sndL
 
 sndL :: Lens (a, b) (a, c) b c
 sndL f = id *** f >>> uncurry (fmap ∘ (,))
 
-swapL :: Lens (a, b) (c, d) (b, a) (d, c)
+swapL :: Iso (a, b) (c, d) (b, a) (d, c)
 swapL = iso swap swap
 
 unitL :: Lens α α () ()
